341835484df3b346ccf7d272437a9d02080e19a3
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-16 18:12:11 -0400

    added puts() prototype

--- include/stdio.h
+++ include/stdio.h
@@ -16,6 +16,11 @@
 #ifndef BAREMETAL_LIBC_STDIO_H
 #define BAREMETAL_LIBC_STDIO_H
 
+/// @brief An indicator that the end of the
+/// file has been reached.
+/// @ingroup baremetal-stdio
+#define EOF (-1)
+
 /// @brief Move the file position relative
 /// to the beginning of the file.
 /// @ingroup baremetal-stdio
@@ -30,5 +35,22 @@
 /// to the end of the file.
 /// @ingroup baremetal-stdio
 #define SEEK_END 2
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// @brief Writes a string to the
+/// default output.
+/// @param str The string to write to
+/// the standard output.
+/// @returns Zero on success, EOF if
+/// an error occured.
+/// @ingroup baremetal-stdio
+int puts(const char *str);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif // BAREMETAL_LIBC_STDIO_H
